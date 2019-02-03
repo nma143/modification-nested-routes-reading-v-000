feb9ac356218cd9553e034fc11e80d414c4d0e99
Nikki A <nikki.apostolakis@gmail.com>
2019-02-03 16:01:40 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   resources :authors, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :new, :edit]
+
   end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
