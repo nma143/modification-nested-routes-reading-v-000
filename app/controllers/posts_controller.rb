36f59bacefbe3ef27151552d1168276bbaa2aeb0
Nikki A <nikki.apostolakis@gmail.com>
2019-02-03 15:30:58 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,8 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    # @post = Post.new
+    @post = Post.new(author_id: params[:author_id])
   end
 
   def create
